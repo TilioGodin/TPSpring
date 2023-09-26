3bfb2031b468439d76644621b10d2400bec2f20c
Tilio Godin <tilio.godin@etudiant.univ-rennes1.fr>
2023-09-26 15:58:18 +0200

    initial commit

---
+++ src/test/java/fr/istic/TPSpring/TpSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.istic.TPSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
